4456a3b449d66840f91c0727dcbc37dee699f476
Hectorolon <hrolon@ucol.mx>
2018-05-11 23:02:09 -0500

    first commit

---
+++ ListasDobles/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace ListasDobles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLista = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnEliminarInicio = new System.Windows.Forms.Button();
+            this.btnEliminarUltimo = new System.Windows.Forms.Button();
+            this.btnInvertirLista = new System.Windows.Forms.Button();
+            this.txtMostrar = new System.Windows.Forms.TextBox();
+            this.txtEliminar = new System.Windows.Forms.TextBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(77, 19);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(180, 20);
+            this.txtCodigo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(263, 16);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(263, 48);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(263, 82);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnLista
+            // 
+            this.btnLista.Location = new System.Drawing.Point(357, 154);
+            this.btnLista.Name = "btnLista";
+            this.btnLista.Size = new System.Drawing.Size(75, 23);
+            this.btnLista.TabIndex = 5;
+            this.btnLista.Text = "Listar";
+            this.btnLista.UseVisualStyleBackColor = true;
+            this.btnLista.Click += new System.EventHandler(this.btnLista_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Codigo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Descripcion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cantidad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Precio";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(77, 50);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(180, 20);
+            this.txtNombre.TabIndex = 10;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(77, 84);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(180, 20);
+            this.txtDescripcion.TabIndex = 11;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(77, 120);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(180, 20);
+            this.txtCantidad.TabIndex = 12;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(77, 157);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(180, 20);
+            this.txtPrecio.TabIndex = 13;
+            // 
+            // btnEliminarInicio
+            // 
+            this.btnEliminarInicio.Location = new System.Drawing.Point(263, 118);
+            this.btnEliminarInicio.Name = "btnEliminarInicio";
+            this.btnEliminarInicio.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarInicio.TabIndex = 14;
+            this.btnEliminarInicio.Text = "EliminarInicio";
+            this.btnEliminarInicio.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarUltimo
+            // 
+            this.btnEliminarUltimo.Location = new System.Drawing.Point(263, 154);
+            this.btnEliminarUltimo.Name = "btnEliminarUltimo";
+            this.btnEliminarUltimo.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarUltimo.TabIndex = 15;
+            this.btnEliminarUltimo.Text = "EliminarUltimo";
+            this.btnEliminarUltimo.UseVisualStyleBackColor = true;
+            // 
+            // btnInvertirLista
+            // 
+            this.btnInvertirLista.Location = new System.Drawing.Point(357, 117);
+            this.btnInvertirLista.Name = "btnInvertirLista";
+            this.btnInvertirLista.Size = new System.Drawing.Size(75, 23);
+            this.btnInvertirLista.TabIndex = 16;
+            this.btnInvertirLista.Text = "InvertirLista";
+            this.btnInvertirLista.UseVisualStyleBackColor = true;
+            this.btnInvertirLista.Click += new System.EventHandler(this.btnInvertirLista_Click);
+            // 
+            // txtMostrar
+            // 
+            this.txtMostrar.Location = new System.Drawing.Point(15, 195);
+            this.txtMostrar.Multiline = true;
+            this.txtMostrar.Name = "txtMostrar";
+            this.txtMostrar.Size = new System.Drawing.Size(417, 148);
+            this.txtMostrar.TabIndex = 17;
+            // 
+            // txtEliminar
+            // 
+            this.txtEliminar.Location = new System.Drawing.Point(357, 84);
+            this.txtEliminar.Name = "txtEliminar";
+            this.txtEliminar.Size = new System.Drawing.Size(75, 20);
+            this.txtEliminar.TabIndex = 18;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(357, 50);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(75, 20);
+            this.txtBuscar.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 365);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.txtEliminar);
+            this.Controls.Add(this.txtMostrar);
+            this.Controls.Add(this.btnInvertirLista);
+            this.Controls.Add(this.btnEliminarUltimo);
+            this.Controls.Add(this.btnEliminarInicio);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLista);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCodigo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnLista;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnEliminarInicio;
+        private System.Windows.Forms.Button btnEliminarUltimo;
+        private System.Windows.Forms.Button btnInvertirLista;
+        private System.Windows.Forms.TextBox txtMostrar;
+        private System.Windows.Forms.TextBox txtEliminar;
+        private System.Windows.Forms.TextBox txtBuscar;
+    }
+}
+
---
+++ ListasDobles/Inventario.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDobles
+{
+    class Inventario
+    {
+        private Producto[] _producto;
+        private int _contador;
+
+
+        public Inventario(int tamaño)
+        {
+            _producto = new Producto[tamaño];
+            _contador = 0;
+        }
+
+        public void agregar(Producto nuevo)
+        {
+            if (inicio == null)
+            {
+                inicio = nuevo;
+                ultimo = inicio;
+            }
+            else
+            {
+                ultimo._siguiente = nuevo;
+                nuevo._anterior = ultimo;
+                ultimo = nuevo;
+            }
+        }
+
+        public string eliminar(int posicion)
+        {
+            for (int i = posicion; i < _contador - 1; i++)
+            {
+                _producto[i] = _producto[i + 1];
+            }
+            if (_contador - 1 > 0)
+            {
+                _producto[_contador - 1] = null;
+                _contador--;
+            }
+            return "Producto eliminado";
+        }
+
+        public Producto buscar(int codigo)
+        {
+            for (int i = 0; i < _contador; i++)
+            {
+                if (_producto[i] != null)
+                {
+                    if (_producto[i]._codigo == codigo)
+                        return _producto[i];
+                }
+            }
+            return null;
+        }
+
+        public string listar()
+        {
+            string vector = "";
+            for (int i = 0; i < _contador; i++)
+            {
+                vector += _producto[i].ToString();
+            }
+            return vector;
+        }
+
+        Producto inicio = null;
+        Producto ultimo = null;
+
+        public void agregar(Producto nuevo)
+        {
+            if (inicio == null)
+            {
+                inicio = nuevo;
+                ultimo = inicio;
+            }
+            else
+            {
+                ultimo._siguiente = nuevo;
+                nuevo._anterior = ultimo;
+                ultimo = nuevo;
+            }
+        }
+
+        public string invertirLista()
+        {
+            string res = "";
+            Producto t = ultimo;
+            while(t != null)
+            {
+                res += t.ToString() + Environment.NewLine;
+                t = t._anterior;
+            }
+            return res;
+        }
+    }
+}
